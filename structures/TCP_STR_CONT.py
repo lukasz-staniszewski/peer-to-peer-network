c0efee4714792a85e3f3edb4084c188a47c440a8
Albert Ściseł <a.scisel@stud.elka.pw.edu.pl>
2022-01-04 19:18:38 +0100

    Upload New File

---
+++ structures/TCP_STR_CONT.py
@@ -0,0 +1,7 @@
+class TCP_STR_CONT:
+    def __init__(self, ip_address, port,
+                 file_name, data):
+        self.ip_address = ip_address
+        self.port = port
+        self.file_name = file_name
+        self.data = data
